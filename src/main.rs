d9dc58548fcfa50e9223af90b66650573713c970
David Kurka <david.kurka@gmail.com>
2022-12-09 00:38:26 -0300

    solution to day 2.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 fn main() {
     println!("hello world!");
     solve1();
+    solve2();
 }
 
 
@@ -39,6 +40,65 @@ fn day1b(calories_list: &Vec<i32>) -> i32 {
 
 }
 
+struct RPSPlay {
+    first_move: char,
+    second_move: char,
+}
+
+fn solve2() {
+    let input = parse2(fs::read_to_string("src/input02.in").unwrap());
+    println!("Day 2:");
+    println!("{}", day2a(&input));
+    println!("{}", day2b(&input))
+}
+
+fn parse2(input: String) -> Vec<RPSPlay> {
+    input
+        .lines()
+        .map(|line| line.split_whitespace())
+        .map(|mut line_parts| RPSPlay{first_move: line_parts.next().unwrap().chars().next().unwrap(),
+                                  second_move: line_parts.next().unwrap().chars().next().unwrap()})
+        .collect()
+}
+
+fn day2a(strategy_guide: &Vec<RPSPlay>) -> i32 {
+    strategy_guide
+        .iter()
+        .map(|strategy| match (strategy.first_move, strategy.second_move) {
+            ('A', 'X') => 1+3,
+            ('A', 'Y') => 2+6,
+            ('A', 'Z') => 3+0,
+            ('B', 'X') => 1+0,
+            ('B', 'Y') => 2+3,
+            ('B', 'Z') => 3+6,
+            ('C', 'X') => 1+6,
+            ('C', 'Y') => 2+0,
+            ('C', 'Z') => 3+3,
+            (_, _) => panic!(),
+        }
+        )
+        .sum()
+}
+
+fn day2b(strategy_guide: &Vec<RPSPlay>) -> i32 {
+    strategy_guide
+        .iter()
+        .map(|strategy| match (strategy.first_move, strategy.second_move) {
+            ('A', 'X') => 3+0,
+            ('A', 'Y') => 1+3,
+            ('A', 'Z') => 2+6,
+            ('B', 'X') => 1+0,
+            ('B', 'Y') => 2+3,
+            ('B', 'Z') => 3+6,
+            ('C', 'X') => 2+0,
+            ('C', 'Y') => 3+3,
+            ('C', 'Z') => 1+6,
+            (_, _) => panic!(),
+        }
+        )
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,4 +126,15 @@ mod tests {
         assert_eq!(24000, day1a(&input));
         assert_eq!(45000, day1b(&input));
     }
+
+    #[test]
+    fn test_day2() {
+        let sample = "\
+A Y
+B X
+C Z";
+        let input = parse2(sample.to_string());
+        assert_eq!(15, day2a(&input));
+        assert_eq!(12, day2b(&input));
+}
 }

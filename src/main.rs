1e4124c77e97c3977f8cd0fc1edb1be1e9949c3a
David Kurka <david.kurka@gmail.com>
2023-03-04 21:14:17 -0300

    adding some time analysis to main function.
    I wonder if I will be able to run the 25 days under 1s!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 pub mod day01;
 pub mod day02;
 pub mod day03;
@@ -11,15 +13,37 @@ pub mod day10;
 pub mod day11;
 
 fn main() {
-    day01::solve();
-    day02::solve();
-    day03::solve();
-    day04::solve();
-    day05::solve();
-    day06::solve();
-    day07::solve();
-    day08::solve();
-    day09::solve();
-    day10::solve();
-    day11::solve();
+    let days = [
+        day01::solve,
+        day02::solve,
+        day03::solve,
+        day04::solve,
+        day05::solve,
+        day06::solve,
+        day07::solve,
+        day08::solve,
+        day09::solve,
+        day10::solve,
+        day11::solve,
+    ];
+
+    let time = Instant::now();
+    let durations = days.map(|day| {
+        let start = time.elapsed();
+        day();
+        let end = time.elapsed();
+        end - start
+    });
+    println!("\n\nTime analysis:");
+    let total_duration: u128 = durations.iter().map(|d| d.as_micros()).sum();
+
+    println!("Total: {:.3}ms", total_duration as f32 / 1000.0);
+    for (day, duration) in durations.iter().enumerate() {
+        println!(
+            "Day {:02}:\t{:>8.3}ms\t{:>5.2}%",
+            day + 1,
+            (duration.as_micros() as f32) / 1000.0,
+            (duration.as_micros() as f32 / total_duration as f32) * 100.0
+        )
+    }
 }

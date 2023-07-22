cb5f96f1e6416e80ba6413748fdd028d092316f8
David Kurka <david.kurka@gmail.com>
2023-07-22 20:27:21 -0300

    A* solution to day12 (2x faster)

--- src/day12.rs
+++ src/day12.rs
@@ -1,5 +1,6 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap},
     fs,
 };
 
@@ -14,20 +15,22 @@ fn parse_input(input: String) -> Vec<Vec<char>> {
     input.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn find_shortest_path(map: &Vec<Vec<char>>, start_pos: (usize, usize)) -> Result<usize, String> {
+fn find_shortest_path(
+    map: &Vec<Vec<char>>,
+    start_pos: (usize, usize),
+    end_pos: (i32, i32),
+) -> Result<usize, String> {
     let mut known_locations: HashMap<(usize, usize), usize> = HashMap::new();
-    let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
+    let mut queue: BinaryHeap<((Reverse<usize>, usize), (usize, usize))> = BinaryHeap::new();
 
     known_locations.insert(start_pos, 0);
-    queue.push_back(start_pos);
+    queue.push(((Reverse(0), 0), start_pos));
 
     let nrows = map.len();
     let ncols = map[0].len();
 
-    while !queue.is_empty() {
+    while let Some(((_, dist), (srow, scol))) = queue.pop() {
         // go to next unvisited spot in queue
-        let (srow, scol) = queue.pop_front().unwrap(); // FUTURE: transform that into A*
-        let dist = *known_locations.get(&(srow, scol)).unwrap();
         if map[srow][scol] == 'E' {
             return Ok(dist);
         }
@@ -40,7 +43,8 @@ fn find_shortest_path(map: &Vec<Vec<char>>, start_pos: (usize, usize)) -> Result
 
         // check neighbors
         for (rdiff, cdiff) in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
-            let (ineirow, ineicol) = ((srow as i32 + rdiff), (scol as i32 + cdiff));
+            let (isrow, iscol) = (srow as i32, scol as i32);
+            let (ineirow, ineicol) = ((isrow + rdiff), (iscol + cdiff));
             if ineirow < 0 || ineirow >= nrows as i32 || ineicol < 0 || ineicol >= ncols as i32 {
                 continue;
             }
@@ -50,17 +54,19 @@ fn find_shortest_path(map: &Vec<Vec<char>>, start_pos: (usize, usize)) -> Result
                 'E' => 'z',
                 c => c,
             };
-            let maybe_known_dist = known_locations.get(&nei_pos);
             if nei_char > (cur_char as u8 + 1) as char {
                 continue;
             }
-            if let Some(known_dist) = maybe_known_dist {
-                if *known_dist <= dist + 1 {
+            let f = dist + 1;
+            let g = ((end_pos.0 - ineirow).abs() + (end_pos.1 - ineicol).abs()) as usize;
+            let maybe_known_score = known_locations.get(&nei_pos);
+            if let Some(known_score) = maybe_known_score {
+                if *known_score <= f + g {
                     continue;
                 }
             }
-            known_locations.insert(nei_pos, dist + 1);
-            queue.push_back(nei_pos)
+            known_locations.insert(nei_pos, f + g);
+            queue.push(((Reverse(f + g), f), nei_pos))
         }
     }
 
@@ -78,10 +84,19 @@ fn solve_12(map: &Vec<Vec<char>>, partb: bool) -> usize {
         }
     }
 
+    // find end position
+    let mut end: (i32, i32) = (0, 0);
+    for row in 0..map.len() {
+        for col in 0..map[row].len() {
+            if map[row][col] == 'E' {
+                end = (row as i32, col as i32);
+            }
+        }
+    }
     // for each start, find shortes path. Returns minimum path found.
     starts
         .iter()
-        .map(|start_pos| find_shortest_path(map, *start_pos))
+        .map(|start_pos| find_shortest_path(map, *start_pos, end))
         .filter_map(|sp| sp.ok())
         .min()
         .unwrap()

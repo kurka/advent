943d3803771f24a39b31fe6c2e440918bd78cd82
DavidKurka <27740573-DavidKurka@users.noreply.replit.com>
2023-11-05 22:48:18 +0000

    More efficient day14 implementation

--- src/day14.rs
+++ src/day14.rs
@@ -49,125 +49,86 @@ fn solve_14(walls: &HashSet<(i32, i32)>, part_a: bool) -> usize {
             max_y = c.1;
         }
     }
-    let solids: &mut HashSet<(i32, i32)> = &mut walls.clone();
+    let solids = &mut walls.clone();
     let start = (500, 0);
     let mut count = 0;
     loop {
-        let mut sand = start;
-        loop {
-            if (part_a && sand.1 > max_y) || (!part_a && solids.contains(&start)) {
-                let mut min_x = 1000000000;
-                let mut max_x = 0;
-                for c in solids.iter() {
-                    if c.0 > max_x {
-                        max_x = c.0
-                    }
-                    if c.0 < min_x {
-                        min_x = c.0
-                    }
-                }
-                println!("{max_y}");
-                for y in 0..=max_y + 2 {
-                    for x in min_x..=max_x {
-                        if walls.contains(&(x, y)) {
-                            print!("#");
-                        } else if solids.contains(&(x, y)) {
-                            print!("o");
-                        } else {
-                            print!(".");
-                        }
-                    }
-                    print!("\n");
-                }
-                return count;
-            }
+        let added = drop(start, solids, max_y, part_a);
+        // println!("{count} {added}");
 
-            if !part_a && sand.1 == max_y + 1 {
-                solids.insert(sand);
-                break;
-            } else if !solids.contains(&(sand.0, sand.1 + 1)) {
-                // move down
-                sand.1 += 1;
-            } else if !solids.contains(&(sand.0 - 1, sand.1 + 1)) {
-                // down blocked, move left
-                sand.0 -= 1;
-                sand.1 += 1;
-            } else if !solids.contains(&(sand.0 + 1, sand.1 + 1)) {
-                sand.0 += 1;
-                sand.1 += 1;
+        count += added;
+
+        if added == 0 || solids.contains(&start) {
+            break;
+        }
+    }
+    /*
+    let mut min_x = 1000000000;
+    let mut max_x = 0;
+    for c in solids.iter() {
+        if c.0 > max_x {
+            max_x = c.0
+        }
+        if c.0 < min_x {
+            min_x = c.0
+        }
+    }
+    for y in 0..=max_y + 2 {
+        for x in min_x..=max_x {
+            if walls.contains(&(x, y)) {
+                print!("#");
+            } else if solids.contains(&(x, y)) {
+                print!("o");
             } else {
-                solids.insert(sand);
-                break;
+                print!(".");
             }
         }
-        count += 1;
+        print!("\n");
     }
+    */
+
+    count
 }
 
-fn look_down(mut sand: (i32, i32), solids: &mut HashSet<(i32, i32)>, max_y: i32) -> bool {
-    loop {
-        sand.1 += 1;
-        if sand.1 > max_y {
-            return false;
-        }
-        // check if there is an object bellow it
-        if solids.contains(&(sand.0, sand.1 + 1)) {
-            // check if left is empty
-            if !solids.contains(&(sand.0 - 1, sand.1 + 1)) {
-                return look_left(sand, solids, max_y);
-            // check if right is empty
-            } else if !solids.contains(&(sand.0 + 1, sand.1 + 1)) {
-                return look_right(sand, solids, max_y);
-            // place sand in middle (left and right busy)
-            } else {
-                solids.insert(sand);
-                return true;
-            }
-        }
+fn drop(sand: (i32, i32), solids: &mut HashSet<(i32, i32)>, max_y: i32, part_a: bool) -> usize {
+    //println!("{:?} {:?}", sand, max_y);
+    if part_a && sand.1 > max_y {
+        return 0;
     }
-}
 
-fn look_left(mut sand: (i32, i32), solids: &mut HashSet<(i32, i32)>, max_y: i32) -> bool {
-    loop {
-        sand.0 -= 1;
-        sand.1 += 1;
-        if sand.1 > max_y {
-            return false;
-        }
-        // check if there is an object bellow it
-        if !solids.contains(&(sand.0, sand.1 + 1)) {
-            return look_down(sand, solids, max_y);
-        // check if left diagonal is busy. If it is, place the sand. Otherwise, continue searching
-        } else if solids.contains(&(sand.0 - 1, sand.1 + 1)) {
-            if !solids.contains(&(sand.0 + 1, sand.1 + 1)) {
-                look_right(sand, solids, max_y);
-            } else {
-                solids.insert(sand);
-                return true;
-            }
-        }
+    if !part_a && sand.1 == max_y + 1 {
+        solids.insert(sand);
+        return 1;
     }
-}
 
-fn look_right(mut sand: (i32, i32), solids: &mut HashSet<(i32, i32)>, max_y: i32) -> bool {
-    loop {
-        sand.0 += 1;
-        sand.1 += 1;
-        if sand.1 > max_y {
-            return false;
-        }
-        // check if there is an object bellow it
-        if !solids.contains(&(sand.0, sand.1 + 1)) {
-            return look_down(sand, solids, max_y);
-        // check if left diagonal is empty, if it is, search left
-        // } else if !solids.contains(&(sand.0 - 1, sand.1 + 1)) {
-        //     look_left(sand, solids, max_y);
-        // finally, check if right diagonal is busy. If it is, place the sand. Otherwise, continue searching
-        } else if solids.contains(&(sand.0 + 1, sand.1 + 1)) {
-            solids.insert(sand);
-            return true;
-        }
+    if !solids.contains(&(sand.0, sand.1 + 1)) {
+        return drop((sand.0, sand.1 + 1), solids, max_y, part_a);
     }
+
+    let mut added = 0;
+    if !solids.contains(&(sand.0 - 1, sand.1 + 1)) {
+        let added_left = drop((sand.0 - 1, sand.1 + 1), solids, max_y, part_a);
+        if added_left == 0 {
+            return 0;
+        }
+        added += added_left;
+    }
+    if solids.contains(&(sand.0 - 1, sand.1 + 1)) && !solids.contains(&(sand.0 + 1, sand.1 + 1)) {
+        let added_right = drop((sand.0 + 1, sand.1 + 1), solids, max_y, part_a);
+        if added_right == 0 {
+            return 0;
+        }
+        added += added_right;
+    }
+
+    if solids.contains(&(sand.0 - 1, sand.1 + 1))
+        && solids.contains(&(sand.0, sand.1 + 1))
+        && solids.contains(&(sand.0 + 1, sand.1 + 1))
+    {
+        solids.insert(sand);
+        added += 1;
+    }
+    return added;
 }
 
 #[cfg(test)]

a083f6782f0e0c1beffe8df4dbbc653f5df0210b
David Kurka <david.kurka@gmail.com>
2023-11-20 22:19:56 -0300

    optimized and correct part_b

--- src/day15.rs
+++ src/day15.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::cmp::{max, min};
+use std::cmp::max;
 use std::collections::HashSet;
 use std::fs;
 
@@ -18,7 +18,7 @@ struct SensorBeacon {
 pub fn solve() {
     let input = parse_input(fs::read_to_string("inputs/input15.in").unwrap());
     println!("Day 15:");
-    println!("{}", solve_part_a(&input, 2000000));
+    // println!("{}", solve_part_a(&input, 2000000));
     println!("{}", solve_part_b(&input, 4000000));
 }
 
@@ -96,7 +96,7 @@ fn solve_part_b(input: &Vec<SensorBeacon>, max_c: i64) -> i64 {
         })
         .collect();
 
-    let mut intersections = vec![];
+    let mut intersections = HashSet::new();
     for d1 in &diamonds {
         for d2 in &diamonds {
             if d1 == d2 {
@@ -104,18 +104,11 @@ fn solve_part_b(input: &Vec<SensorBeacon>, max_c: i64) -> i64 {
             }
             let (d1, r1, u1, l1) = d1;
             let (d2, r2, u2, l2) = d2;
-            let fi1 = find_intersection((u1, r1), (l2, u2));
-            let fi2 = find_intersection2((u1, r1), (l2, u2));
-            if fi1 != None || fi2 != None {
-                println!("{:?} {:?}", (u1, r1), (l2, u2));
-                println!("fi1: {:?}", fi1);
-                println!("fi2: {:?}", fi2);
-            }
             if let Some(i1) = find_intersection((u1, r1), (l2, u2)) {
-                intersections.push(i1);
+                intersections.insert(i1);
             }
             if let Some(i2) = find_intersection((l1, d1), (d2, r2)) {
-                intersections.push(i2);
+                intersections.insert(i2);
             }
         }
     }
@@ -136,7 +129,6 @@ fn solve_part_b(input: &Vec<SensorBeacon>, max_c: i64) -> i64 {
         })
         .collect();
 
-    println!("Res: {res:?}");
     let r = res.get(0).unwrap();
     return r.x * 4000000 + r.y;
 }
@@ -145,109 +137,23 @@ fn find_intersection(l1: (&Point, &Point), l2: (&Point, &Point)) -> Option<Point
     let (Point { x: x1, y: y1 }, Point { x: x2, y: y2 }) = l1;
     let (Point { x: x3, y: y3 }, Point { x: x4, y: y4 }) = l2;
 
-    // x1 = 14
-    // x2 = 16
-    // x3 = 12
-    // x4 = 20
-    //
-    // y1 = 1
-    // y2 = 3
-    // y3 = 1
-    // y4 = -7
-    //
-    //    1 1 1 1 2
-    //    2 4 6 8 0
-    // -7 ........x
-    // -6 .......x.
-    // -5 ......x..
-    // -4 .#...x...
-    // -3 ..#.x....
-    // -2 ...x.....
-    // -1 ..x.#....
-    //  0 .x...#...
-    //  1 x.o...#..
-    //  2 ...o...#.
-    //  3 ....o...#
-    //  4 .........#
-    //  5 .........
-    //
-    // narrow down intersection area
-    // l1 - ur
-    // l2 - lu
-    //
-    if *x3 > *x2 || *x4 < *x1 {
-        return None;
-    }
+    let drift = (*y3 - *y1 - (*x1 - *x3).abs()) / 2;
+    let new_x = max(*x1, *x3) + drift;
+    let new_y = if *x1 >= *x3 { *y1 + drift } else { *y3 - drift };
 
-    if *x3 >= *x1 && *y1 - (*x3 - *x1) >= *y3 && (*y1 - (*x3 - *x1) - *y3) % 2 == 0 {
-        return Some(Point {
-            x: *x3 + (*y1 - (*x3 - *x1) - *y3) / 2,
-            y: *y3 + (*y1 - (*x3 - *x1) - *y3) / 2,
-        });
-    } else if *x1 >= *x3 && *y3 + (*x1 - *x3) <= *y1 && (*y1 - (*x1 - *x3) - *y3) % 2 == 0 {
-        return Some(Point {
-            x: *x1 + (*y1 - (*x1 - *x3) - *y3) / 2,
-            y: *y1 + (*y1 - (*x1 - *x3) - *y3) / 2,
-        });
-    } else {
-        return None;
-    }
-
-    // let x_start = max(*x1, *x3); //14
-    // let x_end = min(*x2, *x4); // 16
-    // let y_start = max(*y1, *y4); // 1
-    // let y_end = min(*y2, *y3); // 1
-
-    // if !((x_start, y_start) == (*x1, *y1) || (x_start, y_end) == (*x3, *y3))
-    //     || x_start > x_end
-    //     || y_start > y_end
-    // {
-    //     // segments don't have intersection
-    //     return None;
-    // }
-
-    // let delta_x = x_end - x_start;
-    // let delta_y = y_end - y_start;
-
-    // if delta_x % 2 != 0 || delta_y % 2 != 0 {
-    //     // intersection happens in fractional coordinates, not in the grid
-    //     return None;
-    // }
-    // if delta_x <= delta_y {
-    //     Some(Point {
-    //         x: x_start + delta_x / 2,
-    //         y: y_start + delta_x / 2,
-    //     })
-    // } else {
-    //     Some(Point {
-    //         x: x_start + delta_y / 2,
-    //         y: y_start + delta_y / 2,
-    //     })
-    // }
-}
-
-fn find_intersection2(l1: (&Point, &Point), l2: (&Point, &Point)) -> Option<Point> {
-    let (Point { x: x1, y: y1 }, Point { x: x2, y: y2 }) = l1;
-    let (Point { x: x3, y: y3 }, Point { x: x4, y: y4 }) = l2;
-
-    let px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4))
-        / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
-    let py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4))
-        / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
-
-    if px >= *x1
-        && px <= *x2
-        && py >= *y1
-        && py <= *y2
-        && px >= *x3
-        && px <= *x4
-        && py >= *y4
-        && py <= *y4
+    if new_x < *x1
+        || new_x > *x2
+        || new_x < *x3
+        || new_x > *x4
+        || new_y < *y1
+        || new_y > *y2
+        || new_y > *y3
+        || new_y < *y4
+        || 2 * drift != *y3 - *y1 - (*x1 - *x3).abs()
     {
-        return Some(Point { x: px, y: py });
-    } else {
         return None;
     }
+    return Some(Point { x: new_x, y: new_y });
 }
 
 #[cfg(test)]

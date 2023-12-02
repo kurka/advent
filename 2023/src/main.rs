30f3ad1353e0305cecd7cbded9751757b68f3ad0
David Kurka <david.kurka@gmail.com>
2023-12-02 12:40:04 -0300

    adding template for 2023

---
+++ 2023/src/main.rs
@@ -0,0 +1,27 @@
+use std::time::Instant;
+
+pub mod day00;
+
+fn main() {
+    let days = [day00::solve];
+
+    let time = Instant::now();
+    let durations = days.map(|day| {
+        let start = time.elapsed();
+        day();
+        let end = time.elapsed();
+        end - start
+    });
+    println!("\n\nTime analysis:");
+    let total_duration: u128 = durations.iter().map(|d| d.as_micros()).sum();
+
+    println!("Total: {:.3}ms", total_duration as f32 / 1000.0);
+    for (day, duration) in durations.iter().enumerate() {
+        println!(
+            "Day {:02}:\t{:>8.3}ms\t{:>5.2}%",
+            day + 1,
+            (duration.as_micros() as f32) / 1000.0,
+            (duration.as_micros() as f32 / total_duration as f32) * 100.0
+        )
+    }
+}

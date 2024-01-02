06b2e5486f1e7c48e6e3ce1e29cc981beb661f21
David Kurka <david.kurka@gmail.com>
2024-01-01 21:47:24 -0300

    day18 solution
    If the path made by the digs is clockwise, we can split horizontal
    segments into two sets: right segments and left segments
    each right segment will have one or more corresponding
    left (sub)-segments, where the distance between them can be filled.
    
    I believe that finding corresponding left segments for right segments
    can be improved if we use a better data structure, but the solution
    right now is O(n^2) or O(n^3), where n is the number of segments. It is
    running fast enough for the AoC inputs, so won't bother changing it now.

--- 2023/src/day18.rs
+++ 2023/src/day18.rs
@@ -1,22 +1,27 @@
 use regex::Regex;
+use std::cmp::{max, min};
 use std::fs;
 
 #[derive(Clone, Debug)]
 struct DigPlan {
     dir: char,
-    steps: i32,
+    steps: i64,
     color: String,
+}
+struct DigPlanSimple {
+    dir: char,
+    steps: i64,
 }
 
 pub fn solve() {
     let input = parse_input(fs::read_to_string("inputs/18.txt").unwrap());
     println!("Day 18:");
     println!("{}", solve_part_a(&input));
-    // println!("{}", solve_part_b(&input));
+    println!("{}", solve_part_b(&input));
 }
 
 fn parse_input(input: String) -> Vec<DigPlan> {
-    let re = Regex::new(r"(\w) (\d+) \((#\w{6})\)").unwrap();
+    let re = Regex::new(r"(\w) (\d+) \(#(\w{6})\)").unwrap();
     input
         .lines()
         .map(|line| {
@@ -30,7 +35,223 @@ fn parse_input(input: String) -> Vec<DigPlan> {
         .collect()
 }
 
-fn solve_part_a(input: &Vec<DigPlan>) -> usize {
+fn solve_part_a(input: &Vec<DigPlan>) -> i64 {
+    // alternative, less efficient solution for part_a:
+    // _solve_part_a_grid(input) as i64
+    solve_18(input, true)
+}
+
+fn solve_part_b(input: &Vec<DigPlan>) -> i64 {
+    solve_18(input, false)
+}
+
+fn solve_18(input: &Vec<DigPlan>, part_a: bool) -> i64 {
+    let mut cur_x = 0;
+    let mut cur_y = 0;
+    let mut prev_x = 0;
+    let mut prev_y = 0;
+
+    let mut coords: Vec<_> = input
+        .iter()
+        .map(|dp| {
+            if part_a {
+                DigPlanSimple {
+                    dir: dp.dir,
+                    steps: dp.steps,
+                }
+            } else {
+                let (dist_str, dir_str) = dp.color.split_at(5);
+                DigPlanSimple {
+                    dir: match dir_str {
+                        "0" => 'R',
+                        "1" => 'D',
+                        "2" => 'L',
+                        "3" => 'U',
+                        x => unreachable!("found: {x}"),
+                    },
+                    steps: i64::from_str_radix(dist_str, 16).unwrap(),
+                }
+            }
+        })
+        .map(|dp| {
+            prev_x = cur_x;
+            prev_y = cur_y;
+            match dp.dir {
+                'L' => cur_x -= dp.steps,
+                'R' => cur_x += dp.steps,
+                'U' => cur_y -= dp.steps,
+                'D' => cur_y += dp.steps,
+                _ => unreachable!(),
+            }
+            let ((nx1, ny1), (nx2, ny2)) = if cur_x >= prev_x {
+                ((prev_x, prev_y), (cur_x, cur_y))
+            } else {
+                ((cur_x, cur_y), (prev_x, prev_y))
+            };
+            ((nx1, ny1), (nx2, ny2), dp.dir)
+        })
+        .collect();
+
+    let min_seg = coords
+        .iter()
+        .filter(|(_, _, d)| *d == 'R' || *d == 'L')
+        .min_by_key(|((_, y), _, _)| y)
+        .unwrap();
+    let start_idx = coords.iter().position(|c| c == min_seg).unwrap();
+    // println!("Min seg: {min_seg:?}; min seg idx: {start_idx:?}");
+    // assert top is right, otherwise invert everything?
+    assert!(min_seg.2 == 'R');
+
+    // bring start to beggining of queue
+    coords.rotate_left(start_idx);
+    // add vertical before horizontal
+    coords.rotate_right(1);
+
+    let mut rights = vec![];
+    let mut lefts = vec![];
+    let mut total = 0;
+    let mut prev_seg = coords[coords.len() - 1];
+    for seg_chunk in coords.chunks_exact(2) {
+        let (vert_seg, hor_seg) = if let [vert_seg, hor_seg] = seg_chunk {
+            (vert_seg, hor_seg)
+        } else {
+            unreachable!()
+        };
+        let &(_p1v, _p2v, dv) = vert_seg;
+        let &(p1h, p2h, dh) = hor_seg;
+        let (p1p, p2p, dp) = prev_seg;
+
+        match dh {
+            'R' => {
+                match dp {
+                    'L' => {
+                        // --<---
+                        // |
+                        // |
+                        // -->---
+                        if dv == 'D' {
+                            total += p1h.1 - p1p.1 - 1;
+                        }
+                        // -->---
+                        // |
+                        // |
+                        // --<---
+                        rights.push((p1h, p2h));
+                    }
+                    'R' => {
+                        // right to right
+                        match dv {
+                            'U' => {
+                                //      -->---
+                                //      |
+                                //      |
+                                // -->---
+                                if rights.len() > 0 {
+                                    let last = rights.pop().unwrap();
+                                    rights.push((last.0, (last.1 .0 - 1, last.1 .1)));
+                                }
+                                rights.push((p1h, p2h));
+                            }
+                            'D' => {
+                                // -->---
+                                //      |
+                                //      |
+                                //      -->---
+                                rights.push(((p1h.0 + 1, p1h.1), p2h));
+                            }
+                            _ => unreachable!(),
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            'L' => {
+                match dp {
+                    'R' => {
+                        // --<---
+                        //      |
+                        //      |
+                        // -->---
+                        if dv == 'U' {
+                            total += p2p.1 - p2h.1 - 1;
+                        }
+                        // -->---
+                        //      |
+                        //      |
+                        // --<---
+                        lefts.push((p1h, p2h));
+                    }
+                    'L' => {
+                        // left to left
+                        match dv {
+                            'D' => {
+                                //      --<---
+                                //      |
+                                //      |
+                                // --<---
+                                if lefts.len() > 0 {
+                                    let last = lefts.pop().unwrap();
+                                    lefts.push(((last.0 .0 + 1, last.0 .1), last.1));
+                                }
+                                lefts.push((p1h, p2h));
+                            }
+                            'U' => {
+                                // --<---
+                                //      |
+                                //      |
+                                //      --<--
+                                lefts.push((p1h, (p2h.0 - 1, p2h.1)));
+                            }
+                            _ => unreachable!(),
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            _ => unreachable!(),
+        }
+
+        prev_seg = *hor_seg;
+    }
+    // fix last seg if needed
+    if coords[coords.len() - 1].2 == 'R' && coords[0].2 == 'U' && coords[1].2 == 'R' {
+        let last_right = rights.pop().unwrap();
+        rights.push((last_right.0, (last_right.1 .0 - 1, last_right.1 .1)));
+    }
+    if coords[coords.len() - 1].2 == 'L' && coords[0].2 == 'D' && coords[1].2 == 'L' {
+        let last_left = lefts.pop().unwrap();
+        lefts.push(((last_left.0 .0 + 1, last_left.0 .1), last_left.1));
+    }
+
+    // println!("Coords: {coords:?}\nLefts: {lefts:?}\nRights: {rights:?}\ntotal: {total}");
+
+    for right in rights {
+        let ((rx1, ry1), (rx2, _ry2)) = right;
+        // println!("Trying to find matches for {right:?} {}", rx2 - rx1);
+        // find the highest left that aligns with rx1
+        let mut segments = vec![(rx1, rx2)];
+        // O(n**2) or O(n**3) solution. It must be possible to do something better.
+        while let Some((x1, x2)) = segments.pop() {
+            let &((lx1, ly1), (lx2, _ly2)) = lefts
+                .iter()
+                .filter(|((lx1, ly1), (lx2, _ly2))| *ly1 > ry1 && !(*lx2 < x1 || *lx1 > x2))
+                .min_by_key(|((_, y), _)| *y)
+                .unwrap();
+            total += (min(x2, lx2) - max(x1, lx1) + 1) * (ly1 - ry1 + 1);
+            if lx1 > x1 {
+                segments.push((x1, lx1 - 1))
+            }
+            if lx2 < x2 {
+                segments.push((lx2 + 1, x2))
+            }
+            // println!("Right: {right:?} Left: ({lx1}-{lx2} {ly1}={_ly2}) Segments: {segments:?}")
+        }
+    }
+
+    total
+}
+
+fn _solve_part_a_grid(input: &Vec<DigPlan>) -> usize {
     let mut min_x = 0;
     let mut max_x = 0;
     let mut min_y = 0;
@@ -64,17 +285,17 @@ fn solve_part_a(input: &Vec<DigPlan>) -> usize {
     let mut cur_x = -min_x;
     let mut cur_y = -min_y;
     let mut prev_dir = input[input.len() - 1].dir;
-    println!(
-        "grid completed: {min_x}, {max_x};  {min_y}, {max_y} - gx {} gy {}",
-        grid[0].len(),
-        grid.len()
-    );
+    // println!(
+    //     "grid completed: {min_x}, {max_x};  {min_y}, {max_y} - gx {} gy {}",
+    //     grid[0].len(),
+    //     grid.len()
+    // );
     // println!("{grid:?}");
     for dp in input {
-        println!(
-            "Processing {dp:?} ({cur_x}, {cur_y}),  {prev_dir} {}",
-            dp.dir
-        );
+        // println!(
+        //     "Processing {dp:?} ({cur_x}, {cur_y}),  {prev_dir} {}",
+        //     dp.dir
+        // );
 
         match (prev_dir, dp.dir) {
             ('D', 'R') => grid[cur_y as usize][cur_x as usize] = 'L',
@@ -123,9 +344,9 @@ fn solve_part_a(input: &Vec<DigPlan>) -> usize {
         _ => unreachable!(),
     }
     // println!("{grid:?}");
-    for l in &grid {
-        println!("{:?}", l.iter().take(600).collect::<String>());
-    }
+    // for l in &grid {
+    //     println!("{:?}", l.iter().take(600).collect::<String>());
+    // }
 
     let mut enclosed = 0;
     for i in 0..grid.len() {
@@ -147,15 +368,11 @@ fn solve_part_a(input: &Vec<DigPlan>) -> usize {
     }
 
     // grid[267][159] = '*';
-    for l in &grid {
-        println!("{:?}", l.iter().take(600).collect::<String>());
-    }
+    // for l in &grid {
+    //     println!("{:?}", l.iter().take(600).collect::<String>());
+    // }
 
     enclosed
-}
-
-fn _solve_part_b(_input: &Vec<DigPlan>) -> usize {
-    todo!()
 }
 
 #[cfg(test)]
@@ -184,6 +401,6 @@ U 2 (#7a21e3)
         let input = parse_input(sample.to_string());
 
         assert_eq!(solve_part_a(&input), 62);
-        // assert_eq!(solve_part_b(&input), 1337);
+        assert_eq!(solve_part_b(&input), 952408144115);
     }
 }

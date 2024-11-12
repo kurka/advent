eba6466b62611669397fe39227bfd0eb7513c0f9
David Kurka <david.kurka@gmail.com>
2024-11-12 01:20:27 -0300

    day22 part 2
    stored all pieces relationships as a (double-linked) graph and use bfs
    to check for pieces affected at each removal.
    
    part 2 is still a bit slow, but I believe it can be optimized later.
    Perhaps memoization on the bfs can help?

--- 2023/src/day22.rs
+++ 2023/src/day22.rs
@@ -9,6 +9,16 @@ struct Point3D {
     z: usize,
 }
 
+type PieceId = usize;
+type Height = usize;
+
+#[derive(Debug)]
+struct Collisions {
+    is_single_support: bool, // registers if piece is responsible for single supporting another
+    above: Vec<PieceId>,
+    below: Vec<PieceId>,
+}
+
 pub fn solve() {
     let input = parse_input(fs::read_to_string("inputs/22.txt").unwrap());
     println!("Day 22:");
@@ -59,6 +69,47 @@ fn parse_input(input: String) -> Vec<(Point3D, Point3D)> {
 }
 
 fn solve_part_a(input: &Vec<(Point3D, Point3D)>) -> usize {
+    let collision_map = find_pieces_dependency(input);
+    // count the number of pieces that are not a "single child" (i.e., is not
+    // exclusively below) for any upper piece
+    collision_map
+        .values()
+        .filter(|colls| !colls.is_single_support)
+        .count()
+}
+
+fn solve_part_b(input: &Vec<(Point3D, Point3D)>) -> usize {
+    let collision_map = find_pieces_dependency(input);
+    // for each piece, find recursively the amount of pieces that it would
+    // destroy by traversing collision map graph
+    collision_map
+        .iter()
+        .map(|(self_id, self_colls)| {
+            if !self_colls.is_single_support {
+                return 0;
+            }
+            let mut destroyed_pieces = HashSet::new();
+            destroyed_pieces.insert(self_id);
+            let mut queue: Vec<&PieceId> = self_colls.above.iter().collect();
+            while queue.len() > 0 {
+                let piece = queue.remove(0);
+                let piece_neis = collision_map.get(piece).unwrap();
+                if piece_neis
+                    .below
+                    .iter()
+                    .all(|b| destroyed_pieces.contains(b))
+                {
+                    destroyed_pieces.insert(piece);
+                    let above_neis: Vec<&PieceId> = piece_neis.above.iter().collect();
+                    queue.extend(above_neis)
+                }
+            }
+            destroyed_pieces.len() - 1
+        })
+        .sum()
+}
+
+fn find_pieces_dependency(input: &Vec<(Point3D, Point3D)>) -> HashMap<usize, Collisions> {
     // create a grid representing a 2D floor
     let (mut max_x, mut max_y) = (0, 0);
     for (pa, pb) in input {
@@ -69,16 +120,27 @@ fn solve_part_a(input: &Vec<(Point3D, Point3D)>) -> usize {
     }
 
     // store top_view_grid as (piece_id, height) tuple grid
-    let mut top_view_grid: Vec<Vec<(usize, usize)>> = vec![vec![(0, 0); max_y + 1]; max_x + 1];
-    // store the state of each piece (false if free, true if blocking)
-    let mut piece_state: HashMap<usize, bool> = input
+    let mut top_view_grid: Vec<Vec<(PieceId, Height)>> = vec![vec![(0, 0); max_y + 1]; max_x + 1];
+
+    // maps piece to all pieces on top of it, and if it is single support (true) or not
+    // let mut collision_map: HashMap<usize, (&bool, Vec<usize>, Vec<usize>> = input
+    let mut collision_map: HashMap<PieceId, Collisions> = input
         .iter()
         .enumerate()
-        .map(|(i, _)| (i + 1, false))
+        .map(|(i, _)| {
+            (
+                i + 1,
+                Collisions {
+                    is_single_support: false,
+                    above: vec![],
+                    below: vec![],
+                },
+            )
+        })
         .collect();
 
     for (i, (pa, pb)) in input.iter().enumerate() {
-        let mut possible_collisions: HashSet<(usize, usize)> = HashSet::new();
+        let mut possible_collisions: HashSet<(PieceId, Height)> = HashSet::new();
         // check for possible colisions
         for x in pa.x..=pb.x {
             for y in pa.y..=pb.y {
@@ -98,28 +160,24 @@ fn solve_part_a(input: &Vec<(Point3D, Point3D)>) -> usize {
                 top_view_grid[x][y] = (i + 1, max_collision_height + pb.z - pa.z + 1)
             }
         }
-        let real_collisions: Vec<&(usize, usize)> = possible_collisions
+        let real_collisions: Vec<PieceId> = possible_collisions
             .iter()
             .filter(|(_, height)| *height == max_collision_height)
+            .map(|(id, _)| *id)
             .collect();
-        if real_collisions.len() == 1 {
-            piece_state.insert(real_collisions[0].0, true);
+        let colls_len = real_collisions.len();
+        for collision_id in real_collisions {
+            // update collisions below
+            let known_collisions_below = collision_map.get_mut(&collision_id).unwrap();
+            known_collisions_below.above.push(i + 1);
+            if colls_len == 1 {
+                known_collisions_below.is_single_support = true
+            }
+            let known_collisions_self = collision_map.get_mut(&(i + 1)).unwrap();
+            known_collisions_self.below.push(collision_id)
         }
     }
-    // for line in top_view_grid {
-    //     println!("{line:?}");
-    // }
-
-    // println!("{piece_state:?}");
-
-    piece_state
-        .iter()
-        .filter(|(_, isblocking)| !**isblocking)
-        .count()
-}
-
-fn solve_part_b(_input: &Vec<(Point3D, Point3D)>) -> usize {
-    todo!()
+    collision_map
 }
 
 #[cfg(test)]
@@ -141,6 +199,6 @@ mod tests {
         let input = parse_input(sample.to_string());
 
         assert_eq!(solve_part_a(&input), 5);
-        // assert_eq!(solve_part_b(&input), 1337);
+        assert_eq!(solve_part_b(&input), 7);
     }
 }

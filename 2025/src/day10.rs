9dca7bdc5132916e7d1d8571a0dc4eb469944fe8
David Kurka <david.kurka@gmail.com>
2025-12-29 20:16:00 -0300

    restoring original slow solution since it is giving correct results.

--- 2025/src/day10.rs
+++ 2025/src/day10.rs
@@ -111,28 +111,38 @@ fn solve_part_b(input: &DayInput) -> i32 {
         .iter()
         .enumerate()
         .map(|(machine_i, machine)| {
-            println!("Machine {}: {:?} {:?}\n{:?}", machine_i, machine.target, machine.joltage, machine.schemas);
+            println!(
+                "Machine {}: {:?} {:?}\n{:?}",
+                machine_i, machine.target, machine.joltage, machine.schemas
+            );
             let n_lights = machine.target.len();
             let n_buttons = machine.schemas.len();
 
             // sort lights by the number of buttons that control it
             let mut sorted_lights: Vec<usize> = (0..n_lights).collect();
-            sorted_lights.sort_by_key(|light_i|
-                (machine.schemas.iter()
-                    .filter(|button| button.contains(&light_i)).count(), machine.joltage[*light_i]));
+            sorted_lights.sort_by_key(|light_i| {
+                (
+                    machine
+                        .schemas
+                        .iter()
+                        .filter(|button| button.contains(&light_i))
+                        .count(),
+                    machine.joltage[*light_i],
+                )
+            });
 
-            let sorted_lights_map: HashMap<usize, usize> = sorted_lights
-                .into_iter()
-                .enumerate()
-                .collect();
+            let sorted_lights_map: HashMap<usize, usize> =
+                sorted_lights.into_iter().enumerate().collect();
 
             let mut sorted_buttons = machine.schemas.clone();
             sorted_buttons.sort_by_key(|button| {
-                let mut mapped_buttons = button.into_iter().map(|b| *sorted_lights_map.get(b).unwrap()).collect::<Vec<usize>>();
+                let mut mapped_buttons = button
+                    .into_iter()
+                    .map(|b| *sorted_lights_map.get(b).unwrap())
+                    .collect::<Vec<usize>>();
                 mapped_buttons.sort();
                 mapped_buttons
-            }
-            );
+            });
 
             println!("Sorted buttons: {sorted_lights_map:?}\n{sorted_buttons:?}");
 
@@ -148,12 +158,50 @@ fn solve_part_b(input: &DayInput) -> i32 {
             // c -> vector with counts of lights activations (joltage), given
             // |c| = l*1
 
-
             let light_to_buttons: Vec<Vec<bool>> = (0..n_lights)
-                .map(|light_i| sorted_buttons.iter().map(|button_j| button_j.contains(&light_i)).collect())
+                .map(|light_i| {
+                    sorted_buttons
+                        .iter()
+                        .map(|button_j| button_j.contains(&light_i))
+                        .collect()
+                })
                 .collect();
             let mut state = Vec::with_capacity(n_buttons);
-            constrained_search(0, &mut state, n_buttons, &machine.joltage, &light_to_buttons).unwrap()
+            constrained_search(
+                0,
+                &mut state,
+                n_buttons,
+                &machine.joltage,
+                &light_to_buttons,
+            ).unwrap()
+
+            // Faster alternative, but with a bug somewhere that I couldn't find:
+
+            // let mut state = vec![0; n_buttons];
+            // let mut resolutions = vec![false; n_buttons];
+            // let mut ranges: Vec<(i32, i32)> = sorted_buttons
+            //     .iter()
+            //     .map(|button_lights| {
+            //         (
+            //             0,
+            //             button_lights
+            //                 .iter()
+            //                 .map(|light_i| machine.joltage[*light_i])
+            //                 .min()
+            //                 .unwrap() as i32,
+            //         )
+            //     })
+            //     .collect();
+            // constrained_search_inorder(
+            //     &mut state,
+            //     &mut resolutions,
+            //     &mut ranges,
+            //     n_buttons,
+            //     n_lights,
+            //     &machine.joltage,
+            //     &light_to_buttons,
+            // )
+            // .unwrap()
         })
         .sum()
 }
@@ -225,13 +273,129 @@ fn constrained_search(
         state.pop();
         if let Some(res) = maybe_res && best_result.is_none_or(|best_result| best_result > res) {
             best_result = maybe_res;
+         }
+     }
+
+     best_result
+ }
+
+
+fn constrained_search_inorder(
+    state: &mut Vec<i32>,
+    resolutions: &mut Vec<bool>,
+    ranges: &mut Vec<(i32, i32)>,
+    n_buttons: usize,
+    n_lights: usize,
+    target_joltage: &Vec<usize>,
+    light_to_buttons: &Vec<Vec<bool>>,
+) -> Option<i32> {
+    if resolutions.iter().all(|x| *x) {
+        let total = state.iter().sum();
+        println!("Found solution {total} {state:?}");
+        return Some(total);
+    }
+
+    // pick the button with minimum range available
+    let (button, (min_val, max_val)) = ranges
+        .iter()
+        .enumerate()
+        .filter(|(b, _)| !resolutions[*b])
+        .min_by_key(|(_, (min_val, max_val))| max_val - min_val)
+        .unwrap();
+    resolutions[button] = true;
+    // println!("S {button} {resolutions:?} {ranges:?} {state:?}");
+
+    let mut best_result = None;
+    for count in *min_val..=*max_val {
+        state[button] = count;
+        // println!("count: {count}");
+
+        // update ranges
+        let joltage_left_per_light: Vec<i32> = light_to_buttons
+            .iter()
+            .enumerate()
+            .map(|(light_i, button_row)| {
+                let joltage_already_used: i32 = (0..n_buttons)
+                    .map(|b| {
+                        if button_row[b] && resolutions[b] {
+                            state[b]
+                        } else {
+                            0
+                        }
+                    })
+                    .sum();
+                target_joltage[light_i] as i32 - joltage_already_used
+            })
+            .collect();
+        let mut created_contradiction = false;
+        for other_button in 0..n_buttons {
+            // only update ranges of buttons that were not resolved.
+            if resolutions[other_button]
+            // || !(0..n_lights).any(|light_i| {
+            //     light_to_buttons[light_i][button] && light_to_buttons[light_i][other_button]
+            // })
+            {
+                continue;
+            }
+            let other_max_val = (0..n_lights)
+                .filter_map(|light_i| {
+                    if light_to_buttons[light_i][other_button] {
+                        Some(joltage_left_per_light[light_i])
+                    } else {
+                        None
+                    }
+                })
+                .min()
+                .unwrap();
+            let other_min_val = (0..n_lights)
+                .filter_map(|light_i| {
+                    if light_to_buttons[light_i][other_button]
+                        // count the number of unresolved buttons in row
+                        && light_to_buttons[light_i]
+                            .iter()
+                            .enumerate()
+                            .filter(|(button_id, button_active)| {
+                                **button_active && !resolutions[*button_id]
+                            })
+                            .count()
+                            == 1
+                    {
+                        Some(joltage_left_per_light[light_i])
+                    } else {
+                        None
+                    }
+                })
+                .max()
+                .unwrap_or(0);
+            if other_max_val < 0 || other_min_val > other_max_val {
+                // skip this count, as it generates a contradiction elsewhere
+                created_contradiction = true;
+                break;
+            }
+            ranges[other_button] = (other_min_val, other_max_val);
+        }
+        if created_contradiction {
+            continue;
+        }
+
+        let maybe_res = constrained_search_inorder(
+            state,
+            resolutions,
+            ranges,
+            n_buttons,
+            n_lights,
+            target_joltage,
+            light_to_buttons,
+        );
+        // println!("Back from recursion: res == {maybe_res:?}");
+        if maybe_res.is_some_and(|res| best_result.is_none_or(|best_result| best_result > res)) {
+            best_result = maybe_res;
         }
     }
 
+    resolutions[button] = false;
     best_result
 }
-
-
 
 #[cfg(test)]
 mod tests {
